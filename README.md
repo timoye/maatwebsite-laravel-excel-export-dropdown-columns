36146a5ac61bfcd40f123be56da6a4c0583ce475
unknown <tim.soladoye@gmail.com>
2020-11-15 08:02:44 +0100

    define all export fields in constructor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Maatwebsite Laravel Excel Export Dropdown Columns
+[Maatwebsite Laravel Excel](https://github.com/Maatwebsite/Laravel-Excel)
+is a simple, but elegant Laravel wrapper around PhpSpreadsheet exports and imports.
+
+You can have dropdown columns in Laravel Excel Export
+
---
+++ UserExport.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
+
+class UserExport implements FromCollection,WithHeadings,WithEvents
+{
+    protected  $users;
+    protected  $selects;
+    protected  $row_count;
+    protected  $column_count;
+    public function __construct()
+    {
+        $status=['active','pending','disabled'];
+        $departments=['Account','Admin','Ict','Sales'];
+        $selects=[  //selects should have column_name and options
+            ['columns_name'=>'D','options'=>$departments],
+            ['columns_name'=>'E','options'=>$status],
+        ];
+        $this->selects=$selects;
+        $this->row_count=50;//number of rows that will have the dropdown. get layout counts (add 1 to rows for heading row)
+        $this->column_count=5;//number of columns to be auto sized
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        return collect([]);
+    }
+    public function headings(): array
+    {
+        return [
+            'name',
+            'email',
+            'phone',
+            'department',
+            'status',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function registerEvents(): array
+    {
+        return [
+            // handle by a closure.
+            AfterSheet::class => function(AfterSheet $event) {
+                $row_count = $this->row_count;
+                $column_count = $this->column_count;
+                foreach ($this->selects as $select){
+                    $drop_column = $select['columns_name'];
+                    $options = $select['options'];
+                    // set dropdown list for first data row
+                    $validation = $event->sheet->getCell("{$drop_column}2")->getDataValidation();
+                    $validation->setType(DataValidation::TYPE_LIST );
+                    $validation->setErrorStyle(DataValidation::STYLE_INFORMATION );
+                    $validation->setAllowBlank(false);
+                    $validation->setShowInputMessage(true);
+                    $validation->setShowErrorMessage(true);
+                    $validation->setShowDropDown(true);
+                    $validation->setErrorTitle('Input error');
+                    $validation->setError('Value is not in list.');
+                    $validation->setPromptTitle('Pick from list');
+                    $validation->setPrompt('Please pick a value from the drop-down list.');
+                    $validation->setFormula1(sprintf('"%s"',implode(',',$options)));
+
+                    // clone validation to remaining rows
+                    for ($i = 3; $i <= $row_count; $i++) {
+                        $event->sheet->getCell("{$drop_column}{$i}")->setDataValidation(clone $validation);
+                    }
+                    // set columns to autosize
+                    for ($i = 1; $i <= $column_count; $i++) {
+                        $column = Coordinate::stringFromColumnIndex($i);
+                        $event->sheet->getColumnDimension($column)->setAutoSize(true);
+                    }
+                }
+
+            },
+        ];
+    }
+}
